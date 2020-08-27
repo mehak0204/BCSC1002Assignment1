5c8011ec5087f63823d9baa43f825f2c82306e6b
mehak0204 <mehak.agarwal_cs19@gla.ac.in>
2020-08-27 12:35:34 +0530

    formatted Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -4,38 +4,35 @@
  *   Time: 16:49
  *   File: Library.java
  */
-
 package definitions;
 
 import java.util.Arrays;
 
 public class Library {
-    private Book[] totalNumberOfBooksInTheLibrary;
+    private final Book[] totalNumberOfBooksInTheLibrary;
 
-    public Library(Book[] totalNumberOfBooksInTheLibrary){
-        this.totalNumberOfBooksInTheLibrary=totalNumberOfBooksInTheLibrary;
+    public Library(Book[] totalNumberOfBooksInTheLibrary) {
+        this.totalNumberOfBooksInTheLibrary = totalNumberOfBooksInTheLibrary;
     }
 
-    public Library(){
-        this.totalNumberOfBooksInTheLibrary=new Book[10];
-        totalNumberOfBooksInTheLibrary[0]=new Book("COMMERCIAL CORRESPONDENCE","D Antopravin Singh","978-93-89970-32-6");
-        totalNumberOfBooksInTheLibrary[1]=new Book("Taxation of Digital Economy","VIJAY SHEKHAR JHA","978-93-86920-88-1");
-        totalNumberOfBooksInTheLibrary[2]=new Book("FINANCIAL ACCOUNTING","DR. M.V. NAGARAJAN","978-81-945166-0-6");
-        totalNumberOfBooksInTheLibrary[3]=new Book("Management Accounting","Dr. Arvind A. Dhond","978-93-89709-74-2");
-        totalNumberOfBooksInTheLibrary[4]=new Book("Media Studies","Meera Suryanarayanan","978-93-89709-72-8");
-        totalNumberOfBooksInTheLibrary[5]=new Book("Motion Graphics and Visual Effects - I","Vikas Shirode and Shweta Shirode","978-93-89709-71-1");
-        totalNumberOfBooksInTheLibrary[6]=new Book("INTERNATIONAL BUSINESS MANAGEMENT","Dr.N.Eswaran and Dr.K.Vishnupriya","978-81-940480-2-2");
-        totalNumberOfBooksInTheLibrary[7]=new Book("Precise Laboratory Manual of Mathematics - 9","Divya Gupta","978-93-87589-86-5"	);
-        totalNumberOfBooksInTheLibrary[8]=new Book("DATA WAREHOUSING AND DATA MINING","Dr D Udaya Suriya Rajkumar","978-93-89970-31-9");
-        totalNumberOfBooksInTheLibrary[9]=new Book("Online Teaching : A Need","Dr. Shyam Sundar Sharma","978-93-88746-55-7");
-
+    public Library() {
+        this.totalNumberOfBooksInTheLibrary = new Book[10];
+        totalNumberOfBooksInTheLibrary[0] = new Book("COMMERCIAL CORRESPONDENCE", "D Antopravin Singh", "978-93-89970-32-6");
+        totalNumberOfBooksInTheLibrary[1] = new Book("Taxation of Digital Economy", "VIJAY SHEKHAR JHA", "978-93-86920-88-1");
+        totalNumberOfBooksInTheLibrary[2] = new Book("FINANCIAL ACCOUNTING", "DR. M.V. NAGARAJAN", "978-81-945166-0-6");
+        totalNumberOfBooksInTheLibrary[3] = new Book("Management Accounting", "Dr. Arvind A. Dhond", "978-93-89709-74-2");
+        totalNumberOfBooksInTheLibrary[4] = new Book("Media Studies", "Meera Suryanarayanan", "978-93-89709-72-8");
+        totalNumberOfBooksInTheLibrary[5] = new Book("Motion Graphics and Visual Effects - I", "Vikas Shirode and Shweta Shirode", "978-93-89709-71-1");
+        totalNumberOfBooksInTheLibrary[6] = new Book("INTERNATIONAL BUSINESS MANAGEMENT", "Dr.N.Eswaran and Dr.K.Vishnupriya", "978-81-940480-2-2");
+        totalNumberOfBooksInTheLibrary[7] = new Book("Precise Laboratory Manual of Mathematics - 9", "Divya Gupta", "978-93-87589-86-5");
+        totalNumberOfBooksInTheLibrary[8] = new Book("DATA WAREHOUSING AND DATA MINING", "Dr D Udaya Suriya Rajkumar", "978-93-89970-31-9");
+        totalNumberOfBooksInTheLibrary[9] = new Book("Online Teaching : A Need", "Dr. Shyam Sundar Sharma", "978-93-88746-55-7");
     }
 
     /**
      * This method will allow us to add total number of books present in  the library to our front desk.
      */
-
-    public void writeTotalNumberOfBooks(){
+    public void writeTotalNumberOfBooks() {
         System.out.println("1. COMMERCIAL CORRESPONDENCE");
         System.out.println("2. Taxation of Digital Economy");
         System.out.println("3. FINANCIAL ACCOUNTING");
@@ -52,13 +49,9 @@ public class Library {
     /**
      * This method is use to show that the new book is added into the student's library account.
      */
-
-    public void addNewBookToIssue(String name){
+    public void addNewBookToIssue(String name) {
         System.out.println("\"" + name + "\" is now added to your Library's Account.");
-
     }
-
-
 
     @Override
     public String toString() {
